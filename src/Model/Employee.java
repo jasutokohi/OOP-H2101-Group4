34189798bdbe3327b267bfbc1138f69032a58ba8
jasutokohi <lr.jkestacio@mmdc.mcl.edu.ph>
2026-02-06 15:33:58 +0800

    Removal of errors

--- src/Model/Employee.java
+++ src/Model/Employee.java
@@ -1,79 +1,34 @@
 package Model;
 
-public class Employee{
-    private String employeeID;
-    private String employeeName;
-    private String birthday;
-    private String gender;
+public abstract class Employee implements EmployeeOperations{
+    private int employeeID;
     private String lastName;
     private String firstName;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     private String position;
     private String phoneNumber;
-=======
-=======
->>>>>>> Stashed changes
     private String status;
-    private String position;
     private String immediateSupervisor;
     private int basicSalary;
     private double hourlyRate;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     
     //Constructors
     public Employee(){
     }
-<<<<<<< Updated upstream
-    
-    public Employee(String EmployeeID, String EmployeePosition, String LastName, String FirstName, String EmployeeGender) {
-        this.employeeID = EmployeeID;
-        this.position = EmployeePosition;
-        this.lastName = LastName;
-        this.firstName = FirstName;
-        this.gender = EmployeeGender;
-=======
 
     public Employee(int employeeID, String lastName, String firstName, String status, String position, String immediateSupervisor, int basicSalary, double hourlyRate) {
         this.employeeID = employeeID;
         this.lastName = lastName;
         this.firstName = firstName;
-
         this.status = status;
         this.position = position;
         this.immediateSupervisor = immediateSupervisor;
         this.basicSalary = basicSalary;
         this.hourlyRate = hourlyRate;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
     
-    public Employee(String EmployeeID, String EmployeePosition, String LastName, String FirstName, String EmployeeGender, String Birthday, String PhoneNumber){
-        this.employeeID = EmployeeID;
-        this.position = EmployeePosition;
-        this.lastName = LastName;
-        this.firstName = FirstName;
-        this.gender = EmployeeGender;
-        this.birthday = Birthday;
-        this.phoneNumber = PhoneNumber;
-    }
-    
-    //Makes the CSV employee information to string
-    @Override
-    public String toString(){ 
-        return """             
-               Employee ID: """ + employeeID + "\nPosition: " + position + "\nFirst Name: " + firstName + 
-                "\nLast Name: " + lastName + "\nEmployee Gender: " + gender + "\nBirthday: " + birthday +"\nPhone Number: " 
-                + phoneNumber;
-    }
     
     //getter methods
-    public String getEmployeeID() {
+    public int getEmployeeID() {
     return employeeID;
     }
 
@@ -85,24 +40,10 @@ public class Employee{
         return firstName;
     }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-=======
-=======
->>>>>>> Stashed changes
     public String getStatus() {
         return status;
     }
-    
->>>>>>> Stashed changes
+
     public String getPosition() {
         return position;
     }
@@ -110,8 +51,7 @@ public class Employee{
     public String getPhoneNumber(){
         return phoneNumber;
     }
-<<<<<<< Updated upstream
-=======
+
     
     @Override
     public int getBasicSalary(){
@@ -123,15 +63,6 @@ public class Employee{
         return hourlyRate;
     }
     
-    @Override
-    public int getBasicSalary(){
-       return basicSalary; 
-    }
-
-    @Override
-    public double getHourlyRate() {
-        return hourlyRate;
-    }
     
     //Setter methods
     public void setEmployeeID(int employeeID) {
@@ -170,9 +101,4 @@ public class Employee{
                 
     @Override
     public abstract String toString();
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
